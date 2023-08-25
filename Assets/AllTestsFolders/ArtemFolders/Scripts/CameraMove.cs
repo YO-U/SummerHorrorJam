03c41cbf8c3aecdd4458db996abd814d05b3881a
SalamanderXVIII <andreysky@list.ru>
2023-08-25 19:32:12 +0200

    Scene near completion №2

--- Assets/AllTestsFolders/ArtemFolders/Scripts/CameraMove.cs
+++ Assets/AllTestsFolders/ArtemFolders/Scripts/CameraMove.cs
@@ -4,78 +4,82 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    private bool down, mid, left, right, isRotating = false;
-    public GameObject camera;
-    private Quaternion initialRotation;
-    private Quaternion targetRotation;
-    public float rotationSpeed = 2.0f;
+	private bool down, mid, left, right, isRotating = false;
+	public GameObject camera;
+	private Quaternion initialRotation;
+	private Quaternion targetRotation;
+	public float initialRotationSpeed = 5.0f; // Начальная скорость поворота
+	public float endingRotationSpeed = 1.0f;  // Замедление скорости поворота
 
-    private void Start()
-    {
-        mid = true;
-        initialRotation = camera.transform.rotation;
-    }
+	private void Start()
+	{
+		mid = true;
+		initialRotation = camera.transform.rotation;
+	}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.S) && mid == true && !isRotating)
-        {
-           
-            StartCoroutine(RotateCamera(Vector3.right * 45));
-            mid = false;
-            down = true;
-        }
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.S) && mid == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.right * 55));
+			mid = false;
+			down = true;
+		}
 
-        if (Input.GetKeyDown(KeyCode.W) && down == true && !isRotating)
-        {
-            StartCoroutine(RotateCamera(Vector3.right * -45));
-            down = false;
-            mid = true;
-        }
+		if (Input.GetKeyDown(KeyCode.W) && down == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.right * -55));
+			down = false;
+			mid = true;
+		}
 
-        if (Input.GetKeyDown(KeyCode.A) && mid == true && !isRotating)
-        {
-            StartCoroutine(RotateCamera(Vector3.up * -90));
-            mid = false;
-            left = true;
-        }
+		if (Input.GetKeyDown(KeyCode.A) && mid == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.up * -70));
+			mid = false;
+			left = true;
+		}
 
-        if (Input.GetKeyDown(KeyCode.D) && mid == true && !isRotating)
-        {
-            StartCoroutine(RotateCamera(Vector3.up * 90));
-            mid = false;
-            right = true;
-        }
+		if (Input.GetKeyDown(KeyCode.D) && mid == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.up * 70));
+			mid = false;
+			right = true;
+		}
 
-        if (Input.GetKeyDown(KeyCode.A) && right == true && !isRotating)
-        {
-            StartCoroutine(RotateCamera(Vector3.up * -90));
-            right = false;
-            mid = true;
-        }
-        if (Input.GetKeyDown(KeyCode.D) && left == true && !isRotating)
-        {
-            StartCoroutine(RotateCamera(Vector3.up * 90));
-            left = false;
-            mid = true;
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.A) && right == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.up * -70));
+			right = false;
+			mid = true;
+		}
+		if (Input.GetKeyDown(KeyCode.D) && left == true && !isRotating)
+		{
+			StartCoroutine(RotateCamera(Vector3.up * 70));
+			left = false;
+			mid = true;
+		}
+	}
 
-    private IEnumerator RotateCamera(Vector3 rotation)
-    {
-        isRotating = true;
-        Quaternion startRotation = camera.transform.rotation;
-        Quaternion endRotation = startRotation * Quaternion.Euler(rotation);
-        float elapsedTime = 0;
+	private IEnumerator RotateCamera(Vector3 rotation)
+	{
+		isRotating = true;
+		Quaternion startRotation = camera.transform.rotation;
+		Quaternion endRotation = startRotation * Quaternion.Euler(rotation);
+		float elapsedTime = 0;
+		float currentRotationSpeed = initialRotationSpeed;
 
-        while (elapsedTime < rotationSpeed)
-        {
-            camera.transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / rotationSpeed);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
+		while (elapsedTime < currentRotationSpeed)
+		{
+			camera.transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / currentRotationSpeed);
+			elapsedTime += Time.deltaTime;
 
-        camera.transform.rotation = endRotation;
-        isRotating = false;
-    }
+			currentRotationSpeed = Mathf.Lerp(initialRotationSpeed, endingRotationSpeed, elapsedTime / currentRotationSpeed);
+
+			yield return null;
+		}
+
+		camera.transform.rotation = endRotation;
+		isRotating = false;
+	}
 }
