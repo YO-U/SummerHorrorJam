b1004da39486b66419c78ef9b76deb7c6ec58af2
SalamanderXVIII <andreysky@list.ru>
2023-08-26 01:50:39 +0200

    Map update, telephone and TV improvement

--- Assets/AllTestsFolders/AndreyFolders/Scripts/OpenCloseObject.cs
+++ Assets/AllTestsFolders/AndreyFolders/Scripts/OpenCloseObject.cs
@@ -10,17 +10,22 @@ public class OpenCloseObject : MonoBehaviour
 	private bool windowOpened = false;
     private bool tvAvtivated = false;
     private int currentChannel = 1;
-    [SerializeField] private Material news;
-	[SerializeField] private Material sports;
-	[SerializeField] private Material music;
-	[SerializeField] private Material horror;
-	[SerializeField] private Material cartoon;
+	[SerializeField] private GameObject tvScreen;
+	[SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private VideoClip news;
+	[SerializeField] private VideoClip sports;
+	[SerializeField] private VideoClip music;
+	[SerializeField] private VideoClip horror;
+	[SerializeField] private VideoClip cartoon;
+	[SerializeField] private Material deactivated;
+	[SerializeField] private Material activated;
 	[SerializeField] private float moveDistance;
     [SerializeField] private CameraMove cameraMove;
     [SerializeField] private KeyCode interact;
+	[SerializeField] private KeyCode interactSecondary;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -29,6 +34,50 @@ public class OpenCloseObject : MonoBehaviour
     void Update()
     {
         InteractibleActivete();
+		TvChannelSwitch();
+    }
+
+	private void MaterialChange(VideoClip clip)
+	{
+		videoPlayer.clip = clip;
+	}
+
+	private void CurrentChannelCheck()
+	{
+		switch (currentChannel)
+		{
+			case 1:
+				MaterialChange(news);
+				break;
+			case 2:
+				MaterialChange(sports);
+				break;
+			case 3:
+				MaterialChange(music);
+				break;
+			case 4:
+				MaterialChange(horror);
+				break;
+			case 5:
+				MaterialChange(cartoon);
+				break;
+		}
+	}
+
+    private void TvChannelSwitch()
+    {
+        if (Input.GetKeyDown(interactSecondary) && (cameraMove.currentState == "left") && tvAvtivated)
+        {
+			videoPlayer.Stop();
+			if (currentChannel != 5)
+			{
+				currentChannel++;
+			}
+			else
+				currentChannel = 1;
+			CurrentChannelCheck();
+			videoPlayer.Play();
+		}
     }
 
     private void GetAndActivateCurrentInteractable()
@@ -52,18 +101,24 @@ public class OpenCloseObject : MonoBehaviour
 				}
 				break;
 
-            case "right":
-				currentInteractible = GameObject.Find("TV");
+            case "left":
+				currentInteractible = GameObject.Find("TVScreen");
                 if (!tvAvtivated)
                 {
-                    switch (currentChannel)
-                    {
-    
-                    }
+					videoPlayer.Play();
+					CurrentChannelCheck();
+					tvScreen.GetComponent<Renderer>().material = activated;
+					tvAvtivated = true;
                 }
+                else
+                {
+					videoPlayer.Stop();
+					tvScreen.GetComponent<Renderer>().material = deactivated;
+					tvAvtivated = false;
+				}
 				break;
 
-            case "left":
+            case "right":
 				currentInteractible = GameObject.Find("Phone");
 				break;
         }
