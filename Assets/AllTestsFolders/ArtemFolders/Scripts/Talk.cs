41144caba71940cc790e12a4177bcbd48c0cf0b2
YO-U <enoxy7@gmail.com>
2023-08-27 22:59:44 +0200

    HumanTalking
    q skeap
    q switch answer
    space choos answer

--- Assets/AllTestsFolders/ArtemFolders/Scripts/Talk.cs
+++ Assets/AllTestsFolders/ArtemFolders/Scripts/Talk.cs
@@ -3,98 +3,92 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class Talk : MonoBehaviour
 {
-    private HumanWalkToWindow humans;
-    private OpenCloseObject opClWindow;
-    public CanvasGroup cnvs;
-    private bool cnvsChecker=false;
-    public GameObject txtHum;
-    public GameObject quest;
-    public GameObject txtPlayer;
-    private string startTxt = "Здрасте пропустите?";
-    public string[,] allHumTxt = new string[10,4]
-    {
-        {"Зачем?",
-            "Да так ","По приколу","Без прикола"},
-        
-        {"Почему?",
-            "прикол","чо","ж"},
-        
-        {"Для чего?",
-            "па","авп","вяпывап"},
-        
-        {"Схерали?",
-            "яавпяа","пявап","яапяап"},
-        
-        {"С какого?",
-            "тспртспр","яавпяавп","стпртср"},
-        
-        {"А чо?",
-            "тспртс","тспртс","тсрптс"},
-        
-        {"А не чо?",
-            "тспртсрт","тспртсрпт","тсрптс"},
-        
-        {"А еще чо?",
-            "тсрптспрт","тсрптсрт","тспртрпьтоь"},
-        
-        {"Друг?",
-            "яавпявап","стспртсрптр","явапяавпяа"},
-        
-        {"Враг?",
-            "срьсрьо","явапрьвпр","явпитср"}
-    };
+    public GameObject canvas;
+    public TextMeshProUGUI humenSpeak;
+    public GameObject objBtns;
+    public Button disagree;
+    public TextMeshProUGUI textDisagree;
+    public Button quest;
+    public TextMeshProUGUI textQuest;
+    private HumanWalkToWindow human;
+    private OpenCloseObject window;
+    private CameraMove camera;
+    private bool switcher=true;
+    private bool switcherbtns=true;
 
     private void Start()
     {
-        cnvs.alpha = 0f;
-        humans = FindObjectOfType<HumanWalkToWindow>();
-        opClWindow = FindObjectOfType<OpenCloseObject>();
-       
-      //  a = Random.Range(1, 10);
-      //  b = Random.Range(2, 3);
+        canvas.SetActive(false);
+        camera = FindObjectOfType<CameraMove>();
+        human = FindObjectOfType<HumanWalkToWindow>();
+        window = FindObjectOfType<OpenCloseObject>();
     }
 
     private void Update()
     {
-        if (humans.hCreatedCh == true && opClWindow.windowOpened == true)
-        {
-           
-            cnvs.alpha = 1f;
-            cnvsChecker = true;
-            txtHum.GetComponent<TextMeshPro>().text = startTxt;
-            int i = 1;
-            if(i<12 && 0 != i%2)
+        if (camera.mid && human.hCreatedCh && window.windowOpened)
+        { 
+            canvas.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Q) && switcher)
             {
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (switcher)
                 {
-                   
-                    txtHum.SetActive(false);
-                    txtPlayer.SetActive(true);
-               //     quest.GetComponent<TextMeshPro>().text = allHumTxt[a, 1];
-                    i++;
+                    switcher = false;
+                }
+                else
+                {
+                    switcher = true;
                 }
             }
-            if(i<12 && 0 == i%2)
+            if (switcher)
             {
-                if (Input.GetKeyDown(KeyCode.Space))
+                humenSpeak.gameObject.SetActive(true);
+                objBtns.SetActive(false);
+            }
+            else
+            {
+                humenSpeak.gameObject.SetActive(false);//выкл текст
+                objBtns.SetActive(true);//вкл кнопки 
+                if (Input.GetKeyDown(KeyCode.Q) && switcher == false)
                 {
-                    
-                    txtHum.SetActive(true);
-                 //   txtHum.GetComponent<TextMeshPro>().text = allHumTxt[a, b]; 
-                    txtPlayer.SetActive(false);
-                    i++;
+                    if (switcherbtns)
+                    {
+                        switcherbtns = false;
+                    }
+                    else
+                    {
+                        switcherbtns = true;
+                    }
+                }
+                if (switcherbtns)
+                {
+                    textDisagree.text = "   >Disagree";
+                    textQuest.text = "Qwuest";
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                            
+                    }
+                }
+                else
+                {
+                    textDisagree.text = "Disagree";
+                    textQuest.text = "   >Qwuest";
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        switcher = true;
+                    }
                 }
             }
+            
         }
         else
         {
-            cnvs.alpha = 0f;
-            cnvsChecker = false;
+            canvas.SetActive(false);
         }
-        
     }
 }
